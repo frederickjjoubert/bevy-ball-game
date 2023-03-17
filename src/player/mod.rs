9ef10bf5ce147708c1df34c08c0b252dbf1b5eb9
Jacques <20562845+frederickjjoubert@users.noreply.github.com>
2023-03-17 14:56:52 -0700

    added Episode 8 Code

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -5,16 +5,24 @@ use systems::*;
 
 use bevy::prelude::*;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
+            // Configure System Sets
+            .configure_set(MovementSystemSet.before(ConfinementSystemSet))
             // Startup Systems
             .add_startup_system(spawn_player)
             // Systems
-            .add_system(confine_player_movement)
-            .add_system(player_movement)
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
