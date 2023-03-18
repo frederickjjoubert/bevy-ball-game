133e38bb904655f305abf9e27695e3497ec91f7b
Vasiliy Taranov <taranov.vv@gmail.com>
2023-03-18 17:49:48 +0300

    derive Default trait
    btw common practice in rust is using return Self when implementing Default trait

--- src/score/resources.rs
+++ src/score/resources.rs
@@ -1,23 +1,11 @@
 use bevy::prelude::*;
 
-#[derive(Resource)]
+#[derive(Resource, Default)]
 pub struct Score {
     pub value: u32,
 }
 
-impl Default for Score {
-    fn default() -> Score {
-        Score { value: 0 }
-    }
-}
-
-#[derive(Resource, Debug)]
+#[derive(Resource, Debug, Default)]
 pub struct HighScores {
     pub scores: Vec<(String, u32)>,
-}
-
-impl Default for HighScores {
-    fn default() -> HighScores {
-        HighScores { scores: Vec::new() }
-    }
 }
