e4738869050e78ee63e35d4f8dd913aad8b355fc
felri <felipercmelo@gmail.com>
2024-01-23 20:10:19 -0300

    remove comments
    # Conflicts:
    #	src/game/ui/game_over_menu/styles.rs

---
+++ src/game/ui/game_over_menu/styles.rs
@@ -0,0 +1,64 @@
+use bevy::prelude::*;
+
+pub const BACKGROUND_COLOR: Color = Color::rgba(0.25, 0.25, 0.25, 0.5);
+
+pub const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+pub const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+pub const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+pub const GAME_OVER_MENU_STYLE: Style = {
+    let mut style = Style::DEFAULT;
+    style.position_type = PositionType::Absolute; // Needed to display separately from HUD.
+    style.display = Display::Flex;                // Hidden by Default
+    style.justify_content = JustifyContent::Center;
+    style.align_items = AlignItems::Center;
+    style.width = Val::Percent(100.0);
+    style.height = Val::Percent(100.0);
+    style
+};
+
+pub const GAME_OVER_MENU_CONTAINER_STYLE: Style = {
+    let mut style = Style::DEFAULT;
+    style.display = Display::Flex;
+    style.flex_direction = FlexDirection::Column;
+    style.justify_content = JustifyContent::Center;
+    style.align_items = AlignItems::Center;
+    style.width = Val::Px(400.0);
+    style.height = Val::Px(400.0);
+    style.row_gap = Val::Px(8.0);
+    style.column_gap = Val::Px(8.0);
+    style
+};
+
+pub const BUTTON_STYLE: Style = {
+    let mut style = Style::DEFAULT;
+    style.width = Val::Px(200.0);
+    style.height = Val::Px(80.0);
+    style.justify_content = JustifyContent::Center;
+    style.align_items = AlignItems::Center;
+    style
+};
+
+pub fn get_title_text_style(asset_server: &Res<AssetServer>) -> TextStyle {
+    TextStyle {
+        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        font_size: 64.0,
+        color: Color::rgb(1.0, 1.0, 1.0),
+    }
+}
+
+pub fn get_final_score_text_style(asset_server: &Res<AssetServer>) -> TextStyle {
+    TextStyle {
+        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        font_size: 48.0,
+        color: Color::rgb(1.0, 1.0, 1.0),
+    }
+}
+
+pub fn get_button_text_style(asset_server: &Res<AssetServer>) -> TextStyle {
+    TextStyle {
+        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        font_size: 32.0,
+        color: Color::rgb(1.0, 1.0, 1.0),
+    }
+}
